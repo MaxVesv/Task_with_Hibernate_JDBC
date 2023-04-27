a0c23aa01acddc1daa996c759cef4d355ed08849
Maxim <vesvalomn@gmail.com>
2023-04-28 02:12:36 +0400

    FEATURE: CREATE CLASS EXEPTIONS FOR DAO

---
+++ src/main/java/jm/task/core/jdbc/dao/TaskJdbcException.java
@@ -0,0 +1,32 @@
+package jm.task.core.jdbc.dao;
+
+import java.sql.SQLException;
+
+public class TaskJdbcException extends SQLException {
+    public TaskJdbcException(String massage, Boolean statusMsgTaskOrNotTask) {
+        if (statusMsgTaskOrNotTask) {
+            /**
+             * Для всех сообщений исключений, связанных с логикой задачи
+             * Место для вставки обработки исключения по задаче
+             *
+             */
+            System.out.println(massage);
+        } else {
+            StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+            massage = "Для исключения передан неправильный параметр = "
+                    + statusMsgTaskOrNotTask
+                    + "\nИсключение вызванно в методе " + stackTrace[2].getMethodName()
+                    + " из класса: " + stackTrace[2].getClassName()
+                    + "\nСообщение исключения: " + massage;
+            throw new RuntimeException(massage);
+        }
+    }
+
+    public TaskJdbcException(SQLException e) {
+        super(e);
+        System.out.println();
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        System.out.println("Исключение вызвано в методе:" + stackTrace[2].getMethodName() +
+                "\nиз класса: " + stackTrace[2].getClassName());
+    }
+}
