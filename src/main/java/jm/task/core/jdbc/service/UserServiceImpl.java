3022e5b11b45d9bd104c47213824f41605df4b35
Maxim <vesvalomn@gmail.com>
2023-04-29 00:34:59 +0400

    REFACTOR (JDBC): ALL TEST SUCCESS

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.TaskJdbcException;
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
@@ -8,38 +9,38 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
     public void createUsersTable() throws TaskJdbcException {
-        UserDaoJDBCImpl serviceJDBCService = UserDaoJDBCImpl.getDaoInstanceJdbc();
-        serviceJDBCService.createUsersTable();
+        UserDao service = UserDaoJDBCImpl.getDaoInstanceJdbc();
+        service.createUsersTable();
 
     }
 
     public void dropUsersTable() throws TaskJdbcException {
-        UserDaoJDBCImpl serviceJDBCService = UserDaoJDBCImpl.getDaoInstanceJdbc();
-        serviceJDBCService.dropUsersTable();
+        UserDao service = UserDaoJDBCImpl.getDaoInstanceJdbc();
+        service.dropUsersTable();
 
     }
 
     public void saveUser(String name, String lastName, byte age) throws TaskJdbcException {
-        UserDaoJDBCImpl serviceJDBCService = UserDaoJDBCImpl.getDaoInstanceJdbc();
-        serviceJDBCService.saveUser(name, lastName, age);
+        UserDao service = UserDaoJDBCImpl.getDaoInstanceJdbc();
+        service.saveUser(name, lastName, age);
 
     }
 
     public void removeUserById(long id) throws TaskJdbcException {
-        UserDaoJDBCImpl serviceJDBCService = UserDaoJDBCImpl.getDaoInstanceJdbc();
-        serviceJDBCService.removeUserById(id);
+        UserDao service = UserDaoJDBCImpl.getDaoInstanceJdbc();
+        service.removeUserById(id);
 
     }
 
     public List<User> getAllUsers() throws TaskJdbcException {
-        UserDaoJDBCImpl serviceJDBCService = UserDaoJDBCImpl.getDaoInstanceJdbc();
-        List<User> listUser = serviceJDBCService.getAllUsers();
+        UserDao service = UserDaoJDBCImpl.getDaoInstanceJdbc();
+        List<User> listUser = service.getAllUsers();
         return listUser;
     }
 
     public void cleanUsersTable() throws TaskJdbcException {
-        UserDaoJDBCImpl serviceJDBCService = UserDaoJDBCImpl.getDaoInstanceJdbc();
-        serviceJDBCService.cleanUsersTable();
+        UserDao service = UserDaoJDBCImpl.getDaoInstanceJdbc();
+        service.cleanUsersTable();
 
     }
 }
