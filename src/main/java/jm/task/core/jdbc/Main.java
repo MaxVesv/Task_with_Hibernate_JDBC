f4848d4541f9c7e711a1ffb1eb5ffea8ecc74867
Maxim <vesvalomn@gmail.com>
2023-04-29 23:15:27 +0400

    REFACTOR: REFACTOR MAIN FOR TEST LOGIC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,20 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import java.sql.SQLException;
 import java.util.List;
 
-import static java.lang.Thread.sleep;
 
 public class Main {
     public static void main(String[] args) throws SQLException, InterruptedException {
         // реализуйте алгоритм здесь
+
+         /**********************************************************************************************
+         *  ПРИМЕР РЕАЛИЗАЦИИ С JDBC********************************************************************
+         *  ********************************************************************************************
+         */
         UserDaoJDBCImpl userDaoJDBC = UserDaoJDBCImpl.getDaoInstanceJdbc();
 
        /**  Создание таблицы User(ов) */
         userDaoJDBC.createUsersTable();
-
+//
        /**  Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления
        *  должен быть вывод в консоль ( User с именем – name добавлен в базу данных */
        userDaoJDBC.saveUser("Ivan", "Petrov", (byte)13);
@@ -35,6 +41,38 @@ public class Main {
 
        /**  Удаление таблицы */
        userDaoJDBC.dropUsersTable();
+
+        /**
+         *  ********************************************************************************************
+         *  ПРИМЕР РЕАЛИЗАЦИИ С HIBERNATE***************************************************************
+         *  ********************************************************************************************
+         */
+
+        UserDao connect = UserDaoHibernateImpl.getDaoInstanceHubernate();
+
+        /**  Создание таблицы User(ов) */
+        connect.createUsersTable();
+
+        /**  Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления
+         *  должен быть вывод в консоль ( User с именем – name добавлен в базу данных */
+        connect.saveUser("Ivan", "Petrov", (byte)13);
+        connect.saveUser("Sergey", "Suvorov", (byte)16);
+        connect.saveUser("Maxim", "Ivanov", (byte)45);
+        connect.saveUser("Petr", "Sidorov", (byte)73);
+//
+        /**  Получение всех User из базы и вывод в консоль ( должен быть переопределен toString в классе User) */
+        List<User> listUser = connect.getAllUsers();
+        System.out.println("ВЫВОД ВЫВОД ПОЛУЧЕННЫХ ДАННЫХ!");
+        list.stream().forEach (a -> System.out.println(a.toString()));
+//
+        //удаление по id
+//        connect.removeUserById(4);
+//
+//        /**  Очистка таблицы User(ов) */
+//        connect.cleanUsersTable();
+//
+//        /**  Удаление таблицы */
+//        connect.dropUsersTable();
 
     }
 }
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,11 +1,14 @@
 package jm.task.core.jdbc.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
-@Table
+@Entity
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
     private Long id;
 
     @Column
@@ -65,5 +68,18 @@ public class User {
                ", name='" + name + '\'' +
                ", lastName='" + lastName + '\'' +
                ", age=" + age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(lastName, user.lastName) && Objects.equals(age, user.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, lastName, age);
     }
 }
