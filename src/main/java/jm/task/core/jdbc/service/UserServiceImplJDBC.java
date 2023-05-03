5cf3c76d2899d14056c2c159e9ea8f5165d671c0
Maxim <vesvalomn@gmail.com>
2023-05-03 16:30:31 +0400

    REFACTOR (JDBC): REMOVED CREATION NEW INSTANCES OF SERVICE

--- src/main/java/jm/task/core/jdbc/service/UserServiceImplJDBC.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImplJDBC.java
@@ -8,38 +8,39 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImplJDBC implements UserService {
+
+    private UserDao userDao;
+
+    public UserServiceImplJDBC() {
+        this.userDao = new UserDaoJDBCImpl();
+    }
+
     public void createUsersTable() throws TaskJdbcException {
-        UserDao userDao = new UserDaoJDBCImpl();
         userDao.createUsersTable();
 
     }
 
     public void dropUsersTable() throws TaskJdbcException {
-        UserDao userDao = new UserDaoJDBCImpl();
         userDao.dropUsersTable();
 
     }
 
     public void saveUser(String name, String lastName, byte age) throws TaskJdbcException {
-        UserDao userDao = new UserDaoJDBCImpl();
         userDao.saveUser(name, lastName, age);
 
     }
 
     public void removeUserById(long id) throws TaskJdbcException {
-        UserDao userDao = new UserDaoJDBCImpl();
         userDao.removeUserById(id);
 
     }
 
     public List<User> getAllUsers() throws TaskJdbcException {
-        UserDao userDao = new UserDaoJDBCImpl();
         List<User> listUser = userDao.getAllUsers();
         return listUser;
     }
 
     public void cleanUsersTable() throws TaskJdbcException {
-        UserDao userDao = new UserDaoJDBCImpl();
         userDao.cleanUsersTable();
 
     }
